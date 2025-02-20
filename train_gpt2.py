c8e7d50898ce1ac2ddcecbef33a32bcaf17c8654
Phu An Nguyen <150226832+Savoxism@users.noreply.github.com>
2025-02-20 13:09:27 +0700

    Finish whole GPT-2 finetuning pipeline

--- train_gpt2.py
+++ train_gpt2.py
@@ -15,11 +15,12 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
         self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
-                             .view(1, 1, config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size)) # soon to delete after applying flash attention
         
     def forward(self, x):
         B, T, C = x.size() # batch_size, sequence_length, embedding dimensionality (n_embd)
@@ -48,13 +49,13 @@ class MLP(nn.Module):
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
         self.gelu = nn.GELU(approximate='tanh')
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         
     def forward(self, x):
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
         return x
-
 
 class Block(nn.Module):
     
@@ -92,8 +93,26 @@ class GPT(nn.Module):
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         
-    def forward(self, idx):
-        # idsx (B, T)
+        # weight sharing scheme
+        self.transformer.wpe.weight = self.lm_head.weight # copy the reference
+        
+        # init params
+        self.apply(self._init_weights)
+        
+    # IMPORTANT
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        
+    def forward(self, idx, targets=None):
+        # idsx (B, T), T is the sequence length
         B, T = idx.size()
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
         # forward the tokens and positional embeddings
@@ -107,9 +126,11 @@ class GPT(nn.Module):
         # forward the final layer norm and the classifier
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x) # (B, T, vocab_size), this is also the prediction
-        
-        return logits 
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
     
+        return logits, loss
     
     @classmethod
     def from_pretrained(cls, model_type):
@@ -160,6 +181,41 @@ class GPT(nn.Module):
         return model
         
 # -----------------------------------------------------------
+import tiktoken        
+        
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+        
+        # at init load tokens from disk and store them in memory 
+        with open('input.txt','r') as f:
+            text = f.read()
+            
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+        
+        # state
+        self.current_position = 0
+        
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position:self.current_position + B*T + 1] # extract a contiguous chunk of tokens from self.tokens, starting at self.current_position
+        x = (buf[:-1]).view(B, T)
+        y = (buf[1:]).view(B, T)
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bound, reset
+        if self.current_position + B * T + 1 > len(self.tokens):
+            self.current_position = 0
+        return x, y
+        
+        
+        
+# -----------------------------------------------------------
 device = "cpu"
 if torch.cuda.is_available():
     device = "cuda"
@@ -167,12 +223,42 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print("using device:", device)
 
-num_return_sequences = 5
-max_length = 30 
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(1337)
+    
+train_loader = DataLoaderLite(B=4, T=32)
+ 
+# get logits
 # model = GPT.from_pretrained('gpt2')
 model = GPT(GPTConfig())
-model.eval()
 model.to(device)
+
+# optimize !
+optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+for i in range(50):
+    x, y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x, y)
+    loss.backward()
+    optimizer.step()
+    print(f"step {i}, loss: {loss.item()}")
+
+
+
+
+
+
+
+
+
+
+import sys; sys.exit(0)
+
+model.eval()
+num_return_sequences = 5
+max_length = 30 
 
 # prefix tokens
 import tiktoken 
